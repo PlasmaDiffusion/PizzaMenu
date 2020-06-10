da158239234036846c7b3d100f9efe382bb0e44a
PlasmaDiffusion <Scott50000@gmail.com>
2020-06-09 21:09:50 -0400

    Allowed localhost + Proper BG location

--- pizza/settings.py
+++ pizza/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pizza-menu-mockup.herokuapp.com']
+ALLOWED_HOSTS = ['pizza-menu-mockup.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
