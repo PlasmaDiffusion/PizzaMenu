db30099bade4e4c0cc1e669687595a88902f1a8a
PlasmaDiffusion <Scott50000@gmail.com>
2020-12-22 16:33:25 -0500

    Changed Topping Interface to use a dropdown menu

--- orders/static/java/menu.js
+++ orders/static/java/menu.js
@@ -7,16 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
         var tableBodyChildren =
           button.parentElement.parentElement.parentElement.children;
 
+        //Hide the table
         for (let i = 1; i < tableBodyChildren.length; i++) {
           if (tableBodyChildren[i].style.display == "table-row")
             tableBodyChildren[i].style.display = "none";
           else tableBodyChildren[i].style.display = "table-row";
         }
 
-        /*Change button text
-        console.log(button.innerHTML);
-        if (button.innerHTML == " &gt; ") button.innerHTML = " v ";
-        else button.innerHTML = " &gt; ";*/
+        //Update topping menus when opened
+        if (button.innerHTML == " Toppings ") {
+          changeToppingButton(
+            document.getElementById("select-native-Toppings")
+          );
+        } else if (button.innerHTML == " Sub Extras ") {
+          changeToppingButton(
+            document.getElementById("select-native-Sub Extras")
+          );
+        }
       };
 
       //Collapse all menus by default
@@ -28,7 +35,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  //Have a selector for each pizza you have in your cart. (Changes input form buttons)
+  var toppingPizzaSelector = document.getElementById("select-native-Toppings");
+  toppingPizzaSelector.onchange = () => {
+    changeToppingButton(toppingPizzaSelector);
+  };
+  var subExtrasSelector = document.getElementById("select-native-Sub Extras");
+  subExtrasSelector.onchange = () => {
+    changeToppingButton(subExtrasSelector);
+  };
+
   //Show the menu on the left, if there's nothing in the cart. Otherwise it will be on the right. (Desktop view only)
   if (!document.getElementById("cart"))
     document.getElementById("menu").style.float = "left";
 });
+
+function changeToppingButton(selector) {
+  document.querySelectorAll("form").forEach((form) => {
+    if (form.className == "toppingForm") {
+      //Show the button/form
+      form.style.display = "block";
+
+      //Overwrite the form to have the proper main item id (the last parameter)
+      let action = form.action.split("/");
+      action[action.length - 2] = selector.value; //Change second last element which would have the argument
+      form.action = action.join("/");
+      //console.log(form.action);
+    }
+  });
+}
