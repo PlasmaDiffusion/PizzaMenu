18b27b17c2180e42c3bd9d0e8ab014955d2c3ff3
PlasmaDiffusion <Scott50000@gmail.com>
2020-12-23 15:01:12 -0500

    Moved submenu buttons out of tables  + B and C css buttons

--- orders/static/java/menu.js
+++ orders/static/java/menu.js
@@ -4,8 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (button.className == "dropdown ui-btn ui-shadow ui-corner-all") {
       //When clicked, this button shows stuff in the table.
       button.onclick = () => {
-        var tableBodyChildren =
-          button.parentElement.parentElement.parentElement.children;
+        var tableBodyChildren = document.getElementById(button.innerHTML)
+          .children;
+        //button.parentElement.parentElement.parentElement.children;
 
         //Hide the table
         for (let i = 1; i < tableBodyChildren.length; i++) {
@@ -15,41 +16,67 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         //Update topping menus when opened
-        if (button.innerHTML == " Toppings ") {
-          changeToppingButton(
-            document.getElementById("select-native-Toppings")
+        if (button.innerHTML == "Toppings") {
+          let toppingElement = document.getElementById(
+            "select-native-Toppings"
           );
-        } else if (button.innerHTML == " Sub Extras ") {
-          changeToppingButton(
-            document.getElementById("select-native-Sub Extras")
+          toggleDisplay(
+            toppingElement.parentElement.parentElement.parentElement
           );
+          changeToppingButton(toppingElement);
+        } else if (button.innerHTML == "Sub Extras") {
+          let subExtrasElement = document.getElementById(
+            "select-native-Sub Extras"
+          );
+          toggleDisplay(
+            subExtrasElement.parentElement.parentElement.parentElement
+          );
+          changeToppingButton(subExtrasElement);
         }
       };
 
       //Collapse all menus by default
-      var tableBodyChildren =
-        button.parentElement.parentElement.parentElement.children;
+      var tableBodyChildren = document.getElementById(button.innerHTML)
+        .children;
 
       for (let i = 1; i < tableBodyChildren.length; i++)
         tableBodyChildren[i].style.display = "none";
     }
   });
 
-  //Have a selector for each pizza you have in your cart. (Changes input form buttons)
-  var toppingPizzaSelector = document.getElementById("select-native-Toppings");
-  toppingPizzaSelector.onchange = () => {
-    changeToppingButton(toppingPizzaSelector);
-  };
-  var subExtrasSelector = document.getElementById("select-native-Sub Extras");
-  subExtrasSelector.onchange = () => {
-    changeToppingButton(subExtrasSelector);
-  };
+  setupSelectors();
 
   //Show the menu on the left, if there's nothing in the cart. Otherwise it will be on the right. (Desktop view only)
   if (!document.getElementById("cart"))
     document.getElementById("menu").style.float = "left";
 });
 
+//--------------------------------------------------------------------------------------------------------------------------------
+//Global functions
+//--------------------------------------------------------------------------------------------------------------------------------
+
+//Have a selector for each pizza you have in your cart. (Changes input form buttons)
+function setupSelectors() {
+  var toppingPizzaSelector = document.getElementById("select-native-Toppings");
+  toppingPizzaSelector.parentElement.parentElement.parentElement.style.display =
+    "none";
+  toppingPizzaSelector.onchange = () => {
+    changeToppingButton(toppingPizzaSelector);
+  };
+  var subExtrasSelector = document.getElementById("select-native-Sub Extras");
+  subExtrasSelector.parentElement.parentElement.parentElement.style.display =
+    "none";
+  subExtrasSelector.onchange = () => {
+    changeToppingButton(subExtrasSelector);
+  };
+}
+
+//Hide or show the element using the display style
+function toggleDisplay(element) {
+  element.style.display = element.style.display == "block" ? "none" : "block";
+}
+
+//Change the form buttons based on the selector for the chosen pizza/sub
 function changeToppingButton(selector) {
   document.querySelectorAll("form").forEach((form) => {
     if (form.className == "toppingForm") {
